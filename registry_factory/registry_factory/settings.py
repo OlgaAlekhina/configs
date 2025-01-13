ea7b5ed4522ac46ad3c8de1152e7307e28968d54
Ольга Алехина <alekhina.olga@gmail.com>
2025-01-14 01:00:04 +0300

    change database to postgres

--- registry_factory/registry_factory/settings.py
+++ registry_factory/registry_factory/settings.py
@@ -86,16 +86,13 @@ WSGI_APPLICATION = 'registry_factory.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # "default": {
-    #     "ENGINE": "django.db.backends.postgresql",
-    #     "NAME": str(os.getenv('DB_NAME')),
-    #     "USER": str(os.getenv('DB_USER')),
-    #     "PASSWORD": str(os.getenv('DB_PASSWORD')),
-    #     "HOST": str(os.getenv('DB_HOST')),
-    #     "PORT": str(os.getenv('DB_PORT')),
+        "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": str(os.getenv('DB_NAME')),
+        "USER": str(os.getenv('DB_USER')),
+        "PASSWORD": str(os.getenv('DB_PASSWORD')),
+        "HOST": str(os.getenv('DB_HOST')),
+        "PORT": str(os.getenv('DB_PORT')),
     }
 }
 
